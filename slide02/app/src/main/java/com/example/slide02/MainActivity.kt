909a9f94a0d60cbbda7ba6a33b3adf646e4d171e
NTKdz <nguyenthekhoi2003@gmail.com>
2023-09-06 14:07:20 +0700

    add appbar, menu

--- slide02/app/src/main/java/com/example/slide02/MainActivity.kt
+++ slide02/app/src/main/java/com/example/slide02/MainActivity.kt
@@ -1,12 +1,54 @@
 package com.example.slide02
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.slide02.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+        setupNumberPicker();
+    }
+
+    private fun setupNumberPicker() {
+        val numberPicker = binding.selectNum
+        numberPicker.minValue = 0
+        numberPicker.maxValue = 1000
+        numberPicker.wrapSelectorWheel = true
+        numberPicker.setOnValueChangedListener { picker, oldVal, newVal ->
+            val text = "Changed from $oldVal to $newVal"
+            Toast.makeText(this@MainActivity, text, Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
