2796f863fce03146d8a0761049051aaadf267dd7
NTKdz <nguyenthekhoi2003@gmail.com>
2023-09-14 11:34:42 +0700

    update

--- slide04/app/src/main/java/com/example/slide04/MainActivity.kt
+++ slide04/app/src/main/java/com/example/slide04/MainActivity.kt
@@ -1,11 +1,44 @@
 package com.example.slide04
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
+    private lateinit var selection: TextView
+    private val items = arrayOf(
+        "Android", "IPhone", "WindowsMobile",
+        "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X"
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.linear_layout)
+        setContentView(R.layout.spinner)
+        selection = findViewById(R.id.selection)
+        val spin = findViewById<Spinner>(R.id.spinner)
+        spin.onItemSelectedListener = this
+        val aa = ArrayAdapter(this, android.R.layout.simple_spinner_item, items)
+        aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spin.adapter = aa
+
+
+        if (spin.selectedItem == null) {
+            selection.text = "null";
+        }
+    }
+
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        selection.text = items[position];
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+        selection.text = ""
     }
 }
