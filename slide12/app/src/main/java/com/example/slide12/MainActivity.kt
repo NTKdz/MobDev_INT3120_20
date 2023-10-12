eba873aeaabea975a94b0d0caa3f74afd8b5cacf
NTKdz <nguyenthekhoi2003@gmail.com>
2023-10-12 09:22:35 +0700

    update

--- slide12/app/src/main/java/com/example/slide12/MainActivity.kt
+++ slide12/app/src/main/java/com/example/slide12/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.slide12
 
+import android.bluetooth.BluetoothAdapter
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.os.Handler
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
@@ -18,6 +22,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.example.slide12.components.Bluetooth
 import com.example.slide12.components.Camera
 import com.example.slide12.components.Sensor
 import com.example.slide12.components.Telephone
@@ -27,6 +34,22 @@ import com.example.slide12.ui.theme.Slide12Theme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        if (ContextCompat.checkSelfPermission(
+                this@MainActivity,
+                android.Manifest.permission.BLUETOOTH_CONNECT
+            ) == PackageManager.PERMISSION_DENIED
+        ) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                ActivityCompat.requestPermissions(
+                    this@MainActivity,
+                    arrayOf(android.Manifest.permission.BLUETOOTH_CONNECT),
+                    2
+                )
+                return
+            }
+        }
+
         setContent {
             Slide12Theme {
                 // A surface container using the 'background' color from the theme
@@ -55,16 +78,22 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
             Button(onClick = { current = "telephone" }) {
                 Text(text = "Telephone")
             }
+
+        }
+        Row(horizontalArrangement = Arrangement.Center) {
             Button(onClick = { current = "camera" }) {
                 Text(text = "camera")
             }
+            Button(onClick = { current = "bluetooth" }) {
+                Text(text = "bluetooth")
+            }
         }
-
         when (current) {
             "sensor" -> Sensor()
             "wifi" -> Wifi()
             "telephone" -> Telephone()
             "camera" -> Camera()
+            "bluetooth" -> Bluetooth()
         }
     }
 }
--- slide12/app/src/main/java/com/example/slide12/components/Bluetooth.kt
+++ slide12/app/src/main/java/com/example/slide12/components/Bluetooth.kt
@@ -1,2 +1,136 @@
 package com.example.slide12.components
 
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothManager
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.util.Log
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.sp
+@Composable
+fun Bluetooth() {
+    val context = LocalContext.current
+
+    val bluetoothManager = context.getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+    val bluetoothAdapter = bluetoothManager.adapter
+    if(bluetoothAdapter==null){
+        Log.d("fdafs","fasfadfadsf")
+    } else {
+        var isEnabled by remember { mutableStateOf(bluetoothAdapter.isEnabled) }
+        var discoverability by remember {
+            mutableStateOf(
+                when (try {
+                    bluetoothAdapter.scanMode
+                } catch (_: SecurityException) {
+                    BluetoothAdapter.SCAN_MODE_NONE
+                }
+                ) {
+                    BluetoothAdapter.SCAN_MODE_CONNECTABLE_DISCOVERABLE -> "Discoverable"
+                    BluetoothAdapter.SCAN_MODE_CONNECTABLE -> "Connectable"
+                    else -> "None"
+                }
+            )
+        }
+
+        val receiver = object : BroadcastReceiver() {
+            override fun onReceive(context: Context, intent: Intent) {
+                val action = intent.action
+
+                if (action == BluetoothAdapter.ACTION_STATE_CHANGED) {
+                    isEnabled = intent.getIntExtra(
+                        BluetoothAdapter.EXTRA_STATE,
+                        BluetoothAdapter.ERROR
+                    ) == BluetoothAdapter.STATE_ON
+                } else if (action == BluetoothAdapter.ACTION_SCAN_MODE_CHANGED) {
+                    discoverability = when (intent.getIntExtra(
+                        BluetoothAdapter.EXTRA_SCAN_MODE, BluetoothAdapter.ERROR
+                    )) {
+                        BluetoothAdapter.SCAN_MODE_CONNECTABLE_DISCOVERABLE -> "Discoverable"
+                        BluetoothAdapter.SCAN_MODE_CONNECTABLE -> "Connectable"
+                        else -> "None"
+                    }
+                }
+            }
+        }
+
+        DisposableEffect(Unit) {
+            context.registerReceiver(
+                receiver,
+                IntentFilter().apply {
+                    addAction(BluetoothAdapter.ACTION_STATE_CHANGED)
+                    addAction(BluetoothAdapter.ACTION_SCAN_MODE_CHANGED)
+                }
+            )
+
+            onDispose {
+                context.unregisterReceiver(receiver)
+            }
+        }
+
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceAround,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Button(
+                    onClick = {
+                        try {
+                            context.startActivity(Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE))
+                        } catch (_: SecurityException) {
+                            Log.e("Bluetooth", "Can't enable")
+                        }
+                    },
+                    enabled = !isEnabled
+                ) { Text(text = "Enable") }
+
+                Button(
+                    onClick = {
+                        try {
+                            context.startActivity(
+                                Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE).apply {
+                                    putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 10)
+                                }
+                            )
+                        } catch (_: SecurityException) {
+                            Log.e("Bluetooth", "Can't discover")
+                        }
+                    },
+                    enabled = discoverability != "Discoverable"
+                ) { Text(text = "Discover") }
+            }
+
+            Text(
+                text = discoverability,
+                color = when (discoverability) {
+                    "Discoverable" -> Color.Green
+                    "Connectable" -> Color.Yellow
+                    else -> Color.Red
+                },
+                fontSize = 24.sp
+            )
+        }
+    }
+}
